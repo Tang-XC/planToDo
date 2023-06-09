57497b8718a3c80b62918f711077435a9a3be7a3
tangxiaochao <Tang-XC@outlook.com>
2023-06-09 12:01:06 +0800

    测试jenkins

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import Router from './router';
 import store from './store';
 import './index.css';
 import { Provider } from 'react-redux';
-console.log('hello world');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <Router />
